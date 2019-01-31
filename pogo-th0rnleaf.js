83d8ec874b2fef7739910eeb63c641c2f2a2303c
Glitch (discord-bot-castform) <none>
2019-01-31 11:45:13 +0000

    Added superficial effects to the th0rnleaf model

--- pogo-th0rnleaf.js
+++ pogo-th0rnleaf.js
@@ -2,39 +2,6 @@
 // https://old.reddit.com/r/TheSilphRoad/comments/aks0k7/weather_gone_germany/ef7jnwu/
 // https://docs.google.com/spreadsheets/d/1v51qbI1egh6eBTk-NTaRy3Qlx2Y2v9kDYqmvHlmntJE/edit#gid=0 
 
-const labelmap = ({
-  sunny:                  'clear',
-  clear:                  'clear',
-  mostlysunny:            'clear',
-  mostlyclear:            'clear',
-  partlysunny:            'partlycloudy',
-  partlysunnywshowers:    'partlycloudy',
-  partlysunnywtstorms:    'partlycloudy',
-  partlysunnywflurries:   'partlycloudy',
-  partlycloudy:           'partlycloudy',
-  partlycloudywshowers:   'partlycloudy',
-  partlycloudywtstorms:	  'partlycloudy',
-  intermittentclouds:     'partlycloudy',
-  mostlycloudy:           'cloudy',
-  mostlycloudywshowers:   'cloudy',
-  mostlycloudywtstorms:   'cloudy',
-  mostlycloudywflurries:  'cloudy',
-  cloudy:                 'cloudy',
-  hazysunshine:           'cloudy',
-  hazymoonlight:          'cloudy',
-  dreary:                 'cloudy',
-  showers:                'rain',
-  rain:                   'rain',
-  tstorms:                'rain',
-  thunderstorms:          'rain',
-  freezingrain:           'rain',
-  fog:                    'fog',
-  rainandsnow:            'snow',
-  flurries:               'snow',
-  snow:                   'snow',
-  windy:                  'windy',
-}) 
-
 const labelEmotes = {
   clear:        '☀',
   partlycloudy: '⛅',
@@ -46,6 +13,39 @@ const labelEmotes = {
   alert:        '⚠',
   none:         '🚫',
 }
+
+const labelmap = ({
+  sunny:                  { dominant: 'clear',        superficial: {} },
+  clear:                  { dominant: 'clear',        superficial: {} },
+  mostlysunny:            { dominant: 'clear',        superficial: {} },
+  mostlyclear:            { dominant: 'clear',        superficial: {} },
+  partlysunny:            { dominant: 'partlycloudy', superficial: {} },
+  partlysunnywshowers:    { dominant: 'partlycloudy', superficial: { rain: true } },
+  partlysunnywtstorms:    { dominant: 'partlycloudy', superficial: { rain: true } },
+  partlysunnywflurries:   { dominant: 'partlycloudy', superficial: { snow: true } },
+  partlycloudy:           { dominant: 'partlycloudy', superficial: {} },
+  partlycloudywshowers:   { dominant: 'partlycloudy', superficial: { rain: true } },
+  partlycloudywtstorms:	  { dominant: 'partlycloudy', superficial: { rain: true } },
+  intermittentclouds:     { dominant: 'partlycloudy', superficial: {} },
+  mostlycloudy:           { dominant: 'cloudy',       superficial: {} },
+  mostlycloudywshowers:   { dominant: 'cloudy',       superficial: { rain: true } },
+  mostlycloudywtstorms:   { dominant: 'cloudy',       superficial: { rain: true } },
+  mostlycloudywflurries:  { dominant: 'cloudy',       superficial: { snow: true } },
+  cloudy:                 { dominant: 'cloudy',       superficial: {} },
+  hazysunshine:           { dominant: 'cloudy',       superficial: {} },
+  hazymoonlight:          { dominant: 'cloudy',       superficial: {} },
+  dreary:                 { dominant: 'cloudy',       superficial: {} },
+  showers:                { dominant: 'rain',         superficial: {} },
+  rain:                   { dominant: 'rain',         superficial: {} },
+  tstorms:                { dominant: 'rain',         superficial: {} },
+  thunderstorms:          { dominant: 'rain',         superficial: {} },
+  freezingrain:           { dominant: 'rain',         superficial: {} },
+  fog:                    { dominant: 'fog',          superficial: {} },
+  rainandsnow:            { dominant: 'snow',         superficial: {} },
+  flurries:               { dominant: 'snow',         superficial: {} },
+  snow:                   { dominant: 'snow',         superficial: {} },
+  windy:                  { dominant: 'windy',        superficial: {} },
+}) 
 
 const windyList = [
   'clear',
@@ -69,8 +69,8 @@ const thresholds = {
     gust: 31.1,
   },
   superficial: {
-    rain: 50,
-    snow: 50,
+    // rain: 50,
+    // snow: 50,
     wind: 16,
   },
 }
@@ -80,12 +80,15 @@ const aw2pogo = ({
   wind, 
   gust,
 }) => {
-  const boost = labelmap[label]
-  const windyable = windyList.includes(label)
+  const weather = labelmap[label]
+  const windyable = [weather.dominant, ...Object.keys(weather.superficial)].some(label => ['rain', 'snow', 'fog'].includes(label))
   const windy = wind >= thresholds.dominant.wind || gust >= thresholds.dominant.gust
   return {
-    dominant: windyable && windy ? 'windy' : boost,
-    superficial: {}, // NOTE: Skipping for now
+    dominant: windyable && windy ? 'windy' : weather.dominant,
+    superficial: {
+      ...weather.superficial,
+      windy: windy || wind >= thresholds.superficial.wind
+    },
   }
 }
 
