29e5cb2b32814248a5dcafce4b44f8b72c5266e7
Glitch (discord-bot-castform) <none>
2019-02-02 13:41:06 +0000

    Lint

--- server.js
+++ server.js
@@ -6,19 +6,19 @@ const JSONDB = require('node-json-db')
 
 const app = express()
 
-app.get("/", (request, response) => {
+app.get('/', (request, response) => {
   response.sendStatus(200)
 })
 
-app.get("/locations", (request, response) => {
+app.get('/locations', (request, response) => {
   const locations = new JSONDB('locations', true, true).getData('/')
   response.json(locations)
 })
-app.get("/weather/:location", (request, response) => {
+app.get('/weather/:location', (request, response) => {
   const weather = new JSONDB(`weather_${request.params.location}`, true, true).getData('/')
   response.json(weather)
 })
-app.get("/weather/:location/:year-:month-:day", (request, response) => {
+app.get('/weather/:location/:year-:month-:day', (request, response) => {
   const { location, year, month, day } = request.params
   const weather = new JSONDB(`weather_${location}`, true, true).getData('/')
   response.json(filter(date => date.match(RegExp(`^${year || '....'}-${month || '..'}-${day || '..'}$`)), weather))
