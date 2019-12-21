d330f8697c2980bc37023ef77561bbefc023242c
Sayantan Chaudhuri <5310@users.noreply.github.com>
2019-12-21 18:43:14 +0530

    Stripped out all the v2 changes leaving only 8-hourly reports
    No saving AW forecast, or model results, or anything. 
    
    Should enough for #8.

--- index.js
+++ index.js
@@ -60,85 +60,19 @@ run(async () => {
       const forecast = CB.operate(
         CB.map(_ => location),
         aw.query,
-        // CB.tap(console.debug), // DEBUG:
-        CB.tap(weathers => {
-          console.info({
-            timestamp: DateTime.local().setZone(location.timezone).toISO(),
-            info: 'predictions',
-            location: key,
-            payload: weathers.map(({
-              date,
-              hour,
-              label
-            }) => ({
-              [`${date}T${hour}`]: label
-            })).reduce(...flattenObj),
-          })
-          const awDB = new JSONDB(`data/aw/${key}/${weathers[0].querydate}`, true, true)
-          awDB.push(`/${weathers[0].queryhour}`, weathers, true)
-          if (location.model) {
-            const modelDB = new JSONDB(`data/model/${key}/${weathers[0].querydate}`, true, true)
-            modelDB.push(`/${weathers[0].queryhour}`, weathers.map(weather => ({
-              querydate: weather.querydate,
-              queryhour: weather.queryhour,
-              date: weather.date,
-              hour: weather.hour,
-              model: model(weather)
-            })), true)
-          }
-        }),
       )
 
       // reports
       const report = CB.operate(
-        CB.map(_ => DateTime.local().setZone(location.timezone).startOf('hour')),
-        CB.map(now => pipe(
-          [-1, 0],
-          map(x => now.plus({
-            day: x
-          }).toISODate()),
-          map(dt => values(new JSONDB(`data/aw/${key}/${dt}`).getData('/'))),
-          flatten,
-          reduce(
-            (predictions, weather) => {
-              const queryhour = DateTime.fromISO(`${weather.querydate}T${weather.queryhour}`, {
-                zone: location.timezone
-              })
-              const hour = DateTime.fromISO(`${weather.date}T${weather.hour}`, {
-                zone: location.timezone
-              })
-              const toForecast = hour.diff(now).as('hour') - 1
-              const fromQuery = now.diff(queryhour).as('hour')
-              if (toForecast >= 0 && toForecast < 12) {
-                predictions[toForecast].forecasts[fromQuery] = {
-                  queryhour,
-                  weather: model(weather)
-                }
-              }
-              return predictions
-            },
-            range(0, 12, 1).map(x => ({
-              hour: now.plus({
-                hour: x + 1
-              }),
-              forecasts: fill(12 - x, undefined)
-            }))
-          )
-        )),
-        // CB.tap(console.debug), // DEBUG:
-        CB.map(predictions => {
+        CB.map(weathers => {
           const now = DateTime.local().setZone(location.timezone).startOf('hour')
-          const clocks = '🕛 🕐 🕑 🕒 🕓 🕔 🕕 🕖 🕗 🕘 🕙 🕚'.split(' ')
           const report = [
             `**${location.name}** \`${now.toISODate()}T${now.toISOTime().slice(0, 2)}\``,
-            `       ${range(0, 12, 1).map(x => now.minus({ hours: x }).hour % 12).map(hour => clocks[hour]).join('')}`,
-            ...predictions
-              .map(prediction => `\`${prediction.hour.toISOTime().slice(0, 2)}\` ${
-                prediction.forecasts
-                  .map(forecast => pogo.labelEmoteMap[forecast ? forecast.weather.dominant : 'none'])
-                  .join('')
-              }`),
-            '—'
+            weathers.slice(0, 8).map(({hour}) => `\`${hour}\``).join('  '),
+            weathers.slice(0, 8)
+              .map(model)
+              .map(forecast => pogo.labelEmoteMap[forecast ? forecast.dominant : 'none'])
+              .join('  ')
           ]
           return report
         }),
