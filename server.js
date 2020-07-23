784f708e079fe33f43e0ea1aa5b5d4f8d26a00bd
repl.it user <replituser@example.com>
2020-07-23 10:42:41 +0000

    Removed unnecessary config exposure

--- server.js
+++ server.js
@@ -7,10 +7,5 @@ app.get('/', (request, response) => {
   response.sendStatus(200)
 })
 
-app.get('/locations', (request, response) => {
-  const locations = new JSONDB('locations', true, true).getData('/')
-  response.json(Object.keys(locations))
-})
-
 app.listen(process.env.PORT)
 console.log('Keepalive server is running')
